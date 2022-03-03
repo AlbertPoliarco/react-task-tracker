a07992aeec1b1b1d73280b27e97c5e6579b11adc
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-03-02 22:41:36 -0600

    added task and tasks

---
+++ src/components/Task.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { FaTimesCircle } from "react-icons/fa"
+
+const Task = ({ task, onDelete }) => {
+  return (
+    <div className='task'>
+      <h3>
+        {task.text}
+        <FaTimesCircle
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
